c992b5fa4c7738d5ec33562e0521d39a17f164fa
kevin-magu <wanjaukevinmagu@gmail.com>
2023-08-04 01:27:58 +0300

    U

--- src/components/Weathercard.js
+++ src/components/Weathercard.js
@@ -1,51 +1,66 @@
-import React from 'react'
-import  '../styles/Weathercard.css'
-import Clear from '../images/clear.png'
-import Humidity from '../images/humidity.png'
-import Wind from '../images/wind.png'
-import Clouds from '../images/clouds.png'
-import Drizzle from '../images/drizzle.png'
-import Mist from '../images/mist.png'
-import Rain from '../images/rain.png'
-import Snow from '../images/snow.png'
+import React, { useState } from 'react';
+import '../styles/Weathercard.css';
+import Clear from '../images/clear.png';
+import Humidity from '../images/humidity.png';
+import Wind from '../images/wind.png';
 
 function Weathercard() {
   const apiKey = '055b7d5f2715779c9dcb2b24327a0408';
-  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=nairobi';
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-  async function checkWeather (){
-    const response =await fetch(apiUrl + `&appid=${apiKey}`);
-    var data = await response.json()
-    console.log('Data', data)
+  const [city, setCity] = useState('Nairobi');
+  const [temperature, setTemperature] = useState(20);
+  const [humidity, setHumidity] = useState(30);
+  const [wind, setWind] = useState(5);
 
-    document.querySelector(".temperature h1").textContent = Math.round(data.main.temp) + " °C"
-    document.querySelector(".wind").textContent = Math.round(data.wind.speed) + " km/h"
-    document.querySelector(".humidity").textContent = Math.round(data.main.humidity) + " %"
+  async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const data = await response.json();
+    console.log('Data', data);
+
+
+    setCity(data.name);
+    setTemperature(Math.round(data.main.temp));
+    setHumidity(Math.round(data.main.humidity));
+    setWind(Math.round(data.wind.speed));
   }
-checkWeather()
+
+  const handleSearch = () => {
+    checkWeather(city);
+  };
 
   return (
     <div className='weather-card'>
+     
       <div className='search-div'>
-      <input type="text" placeholder='Search Weather by city' />
-      <button>Search</button>
+        <input
+          className='input'
+          type="text"
+          placeholder='Search Weather by city'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button className='btn' onClick={handleSearch}>
+          Search
+        </button>
       </div>
-      <div className='city'><p>Nairobi</p></div>
+      <div className='main-section'>
+      <div className='city'>
+        <p>{city}</p>
+      </div>
       <div className='temperature'>
         <p className='status-image'> <img src={Clear} alt="" /> </p>
-        <h1>20&nbsp; </h1>
+        <h1>{temperature}&nbsp; <sup>oC</sup></h1>
 
         <div className='humidity-wind'>
-          <img src={Humidity} alt="" /> <p className='humidity'>Humidity- 30%</p>
-          <img src={Wind} alt="" /><p className='wind'>Wind-5 km/h</p>
+          <img src={Humidity} alt="" /> <p className='humidity'>Humidity- {humidity}%</p>
+          <img src={Wind} alt="" /><p className='wind'>Wind-{wind} km/h</p>
         </div>
         <p className='author'>Made on earth by humans</p>
       </div>
-
+      </div>
     </div>
-  
-  )
-  }  
+  );
+}
 
-
-export default Weathercard
+export default Weathercard;
